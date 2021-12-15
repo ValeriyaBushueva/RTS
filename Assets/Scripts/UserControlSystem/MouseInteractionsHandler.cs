34d9ad807acc127bfdfa3ab274bb83cafb84070c
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-12-15 23:49:40 +0300

    outline and ui

---
+++ Assets/Scripts/UserControlSystem/MouseInteractionsHandler.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using Abstractions;
+using UnityEngine;
+//using UserControlSystem;
+
+public class MouseInteractionsHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+ //   [SerializeField] private SelectableValue _selectedObject;
+
+
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonUp(0))
+        {
+            return;
+        }
+        var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
+        if (hits.Length == 0)
+        {
+            return;
+        }
+        var mainBuilding = hits
+            .Select(hit => hit.collider.GetComponentInParent<IUnitProducer>())
+            .Where(c => c != null)
+            .FirstOrDefault();
+        if (mainBuilding == default)
+        {
+            return;
+        }
+        mainBuilding.ProduceUnit();
+    }
+}
---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Abstractions;
+using UnityEngine;
+using UserControlSystem;
+using QuickOutline.Scripts;
+
+public sealed class MouseInteractionPresenter : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private SelectableValue _selectedObject;
+    private Outline _outline;
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonUp(0))
+        {
+            return;
+        }
+        var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
+        if (hits.Length == 0)
+        {
+            return;
+        }
+        var selectable = hits
+            .Select(hit => hit.collider.GetComponentInParent<ISelectable>())
+            .FirstOrDefault(c => c != null);
+        _selectedObject.SetValue(selectable);
+        
+        _outline = hits.Select(hit => hit.collider.GetComponentInParent<Outline>()).FirstOrDefault(c => c != null);
+        _outline.enabled = true;
+       
+       
+       
+    }
+}
