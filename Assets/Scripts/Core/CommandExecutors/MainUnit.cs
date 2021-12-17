11a23f3a6b2a7f7d34fec182af38ea32b7ae6fea
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-12-18 00:11:32 +0300

    settings scene

---
+++ Assets/Scripts/Core/CommandExecutors/MainUnit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Abstractions;
+using UnityEngine;
+
+public class MainUnit : MonoBehaviour, ISelectable
+{
+    public float Health => _health; 
+    public float MaxHealth => _maxHealth;
+    public Sprite Icon => _icon;
+    
+    [SerializeField] private float _maxHealth = 100;
+    [SerializeField] private Sprite _icon;
+
+    private float _health = 100;
+}
