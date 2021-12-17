693e279adf568d45444b7cbb0f7f3231e0041143
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-12-18 00:43:36 +0300

    Inheriting the ProduceUnitCommand class and fix injector

---
+++ Assets/Scripts/UserControlSystem/CommandsRealization/ProduceUnitCommandHeir.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Abstractions.Commands.CommandsInterfaces;
+using UnityEngine;
+using UserControlSystem.CommandsRealization;
+
+public class ProduceUnitCommandHeir : ProduceUnitCommand 
+{
+     
+}
--- Assets/Scripts/Utils/AssetsInjector.cs
+++ Assets/Scripts/Utils/AssetsInjector.cs
@@ -6,25 +6,28 @@ namespace Utils
     public static class AssetsInjector
     {
         private static readonly Type _injectAssetAttributeType = typeof(InjectAssetAttribute);
-        
+
         public static T Inject<T>(this AssetsContext context, T target)
         {
             var targetType = target.GetType();
-            var allFields = targetType.GetFields(BindingFlags.NonPublic 
-                                                 | BindingFlags.Public 
-                                                 | BindingFlags.Instance);
-
-            for (int i = 0; i < allFields.Length; i++)
+            while (targetType != null)
             {
-                var fieldInfo = allFields[i];
-                var injectAssetAttribute = fieldInfo.GetCustomAttribute(_injectAssetAttributeType) as InjectAssetAttribute;
-                if (injectAssetAttribute == null)
+                var allFields = targetType.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+                for (int i = 0; i < allFields.Length; i++)
                 {
-                    continue;
+                    var fieldInfo = allFields[i];
+                    var injectAssetAttribute = fieldInfo.GetCustomAttribute(_injectAssetAttributeType) as InjectAssetAttribute;
+                    if (injectAssetAttribute == null)
+                    {
+                        continue;
+                    }
+                    var objectToInject = context.GetObjectOfType(fieldInfo.FieldType, injectAssetAttribute.AssetName);
+                    fieldInfo.SetValue(target, objectToInject);
                 }
-                var objectToInject = context.GetObjectOfType(fieldInfo.FieldType, injectAssetAttribute.AssetName);
-                fieldInfo.SetValue(target, objectToInject);
-            }	
+
+                targetType = targetType.BaseType;
+            }
 
             return target;
         }
