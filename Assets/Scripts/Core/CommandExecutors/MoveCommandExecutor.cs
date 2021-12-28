e7fc70f36f58123035c650a2f6e07a864ca2627c
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-12-29 01:51:08 +0300

    NavMesh Obstacle

--- Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
+++ Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Abstractions.Commands.CommandsInterfaces;
 using UnityEngine;
 using UnityEngine.AI;
@@ -16,9 +17,34 @@ namespace Abstractions.Commands.CommandExecutors
         public override async Task ExecuteSpecificCommand(IMoveCommand command)
         {
             GetComponent<NavMeshAgent>().destination = command.Target;
-           // _animator.SetTrigger("Walk");
-            // await _stop;
-            // _animator.SetTrigger("Idle");
+            _animator.SetTrigger("Walk");
+            await _stop;
+            _animator.SetTrigger("Idle");
+            
+            
+        //     _animator.SetTrigger(Animator.StringToHash(AnimationTypes.Walk));
+        //     _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+        //     try
+        //     {
+        //         await _stop
+        //             .WithCancellation
+        //             (
+        //                 _stopCommandExecutor
+        //                     .CancellationTokenSource
+        //                     .Token
+        //             );
+        //     }
+        //     catch
+        //     {
+        //         GetComponent<NavMeshAgent>().isStopped = true;
+        //         GetComponent<NavMeshAgent>().ResetPath();
+        //     }
+        //     _stopCommandExecutor.CancellationTokenSource = null;
+        //     _animator.SetTrigger(Animator.StringToHash(AnimationTypes.Idle));
+        // }
+
+
+          
 
         }
     }
