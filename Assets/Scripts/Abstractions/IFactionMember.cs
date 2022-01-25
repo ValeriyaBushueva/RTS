22d1fb46f61e51f826a460bda0506f1c1d4c70e9
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2022-01-25 23:00:19 +0300

    add scripts for attacking

---
+++ Assets/Scripts/Abstractions/IFactionMember.cs
@@ -0,0 +1,5 @@
+
+public interface  IFactionMember
+{
+    int FactionId { get; }
+}
--- Assets/Scripts/Core/MainBuilding.cs
+++ Assets/Scripts/Core/MainBuilding.cs
@@ -1,18 +1,35 @@
 using Abstractions;
 using UnityEngine;
 
-public sealed class MainBuilding : MonoBehaviour, ISelectable
+public sealed class MainBuilding : MonoBehaviour, ISelectable, IAttackable
 {
     public float Health => _health;
     public float MaxHealth => _maxHealth;
+
     public Transform PivotPoint => _pivotPoint;
+
     public Sprite Icon => _icon;
 
     [SerializeField] private float _maxHealth = 1000;
+
     [SerializeField] private Sprite _icon;
+
     [SerializeField] private Transform _pivotPoint;
+
     public Vector3 RallyPoint { get; set; }
 
     private float _health = 1000;
 
+    public void RecieveDamage(int amount)
+    {
+        if (_health <= 0)
+        {
+            return;
+        }
+        _health -= amount;
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
